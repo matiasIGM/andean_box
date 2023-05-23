0171360ced1dda8b2adf9d430f9e6e884ec91fe8
Matias Garces Martinez <54512479+matiasIGM@users.noreply.github.com>
2023-05-23 14:47:40 -0400

    afinamiento modelo Envio

--- delivery_project/api_restful/models.py
+++ delivery_project/api_restful/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.utils.crypto import get_random_string
+from django.utils import timezone
+from django.utils.timezone import datetime
 # Create your models here.
 # class Paquete(models.Model):
 #     title = models.CharField(max_length=255, default='blank title')
@@ -32,13 +34,33 @@ class Sucursal(models.Model):
     def __str__(self):
         return self.nombre
 
+# class Envio(models.Model):
+#     numero_seguimiento = models.CharField(max_length=7, unique=True, editable=False)
+#     direccion_origen = models.CharField(max_length=100, default="CD de distribución")
+#     nombre_destinatario = models.CharField(max_length=100)
+#     direccion_destinatario = models.CharField(max_length=100)
+#     fecha_hora_recepcion = models.DateTimeField(null=True, blank=True)
+#     fecha_creacion = models.DateTimeField(default=datetime.now, editable=False)
+#     sucursal_destino = models.ForeignKey(Sucursal, on_delete=models.CASCADE)
 class Envio(models.Model):
     numero_seguimiento = models.CharField(max_length=7, unique=True, editable=False)
-    direccion_origen = models.CharField(max_length=100, default="CD de distribución")
+    direccion_origen = models.CharField(max_length=100, default="CD de distribución ENEA PUDAHUEL")
     nombre_destinatario = models.CharField(max_length=100)
-    direccion_destinatario = models.CharField(max_length=100)
+    direccion_destinatario = models.CharField(max_length=100, blank=True)
+    email_destinatario = models.CharField(max_length=100, null=True, blank=True)
+    height = models.FloatField(null=True, blank=True)
+    width = models.FloatField(null=True, blank=True)
+    length = models.FloatField(null=True, blank=True)
+    weight = models.FloatField(null=True, blank=True)
+    items_count = models.IntegerField(default=1)
+    cellphone = models.CharField(max_length=100, null=True, blank=True)
+    destiny = models.CharField(max_length=100, null=True, blank=True)
+    address_attributes = models.CharField(max_length=100, blank=True)
+    branch_office_id = models.IntegerField(null=True, blank=True)
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
     fecha_hora_recepcion = models.DateTimeField(null=True, blank=True)
-    sucursal_destino = models.ForeignKey(Sucursal, on_delete=models.CASCADE)
+    sucursal_destino = models.ForeignKey(Sucursal, on_delete=models.CASCADE, blank=True, null=True)
+
 
     def __str__(self):
         return self.numero_seguimiento
